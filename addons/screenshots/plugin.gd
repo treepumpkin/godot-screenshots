d25aae8428beda45e6fee9444a0bae03ab84e779
Davide Di Staso <davide.distaso@icloud.com>
2025-10-11 12:14:37 +0200

    Use _disable_plugin() to remove the singleton

--- addons/screenshots/plugin.gd
+++ addons/screenshots/plugin.gd
@@ -6,10 +6,21 @@ const SHUTTER_WINDOW = "res://addons/screenshots/autoload/shutter_window/shutter
 var m_editor_debugger_plugin : EditorDebuggerPlugin = preload("res://addons/screenshots/debugger/editor_debugger_plugin.gd").new()
 var bottom_panel : Control
 
-func _enter_tree():
-	add_debugger_plugin(m_editor_debugger_plugin)
+func _enable_plugin() -> void:
 	add_autoload_singleton("Screenshots", SHUTTER_WINDOW)
+	pass
 
-func _exit_tree():
-	remove_debugger_plugin(m_editor_debugger_plugin)
+
+func _disable_plugin() -> void:
 	remove_autoload_singleton("Screenshots")
+	pass
+
+
+func _enter_tree() -> void:
+	add_debugger_plugin(m_editor_debugger_plugin)
+	pass
+
+
+func _exit_tree() -> void:
+	remove_debugger_plugin(m_editor_debugger_plugin)
+	pass
